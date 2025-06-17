10c928bf18af13ffee0f75391e89e164c85cbb2b
ceci6182 <71692358+ceci6182@users.noreply.github.com>
2025-06-17 22:32:50 +0200

    example for exam

---
+++ LudoTest/FluentExamples.cs
@@ -0,0 +1,21 @@
+﻿using FluentAssertions;
+using LudoAPI.Models;
+
+namespace LudoTest;
+
+public class FluentExamples
+{
+    private readonly Player _player = new(Color.Blue);
+    
+    [Fact]
+    public void Standard_UnitTest()
+    {
+        Assert.Equal(Color.Blue, _player.Color);
+    }
+
+    [Fact]
+    public void Fluent_UnitTest()
+    {
+        _player.Color.Should().Be(Color.Blue);
+    }
+}
